52bbf441ebb84687bb75aaa5c2da561294fecf1b
Andres Ochoa <andressochoaa@gmail.com>
2023-06-15 12:18:50 -0500

    ruteo, url y urls con id para cuando se requiera editar o ver una en especifico.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { ListmascotasComponent } from './components/listmascotas/listmascotas.component';
+import { AddEditMascotaComponent } from './components/add-edit-mascota/add-edit-mascota.component';
+import { ViewMascotasComponent } from './components/view-mascotas/view-mascotas.component';
+
+//Ruteo
+const routes: Routes = [
+  {path: '', redirectTo: 'listaMascotas', pathMatch: 'full' },
+  {path: 'listaMascotas', component: ListmascotasComponent },
+  {path: 'agregarMascota', component: AddEditMascotaComponent },
+  {path: 'verMascota/:id', component: ViewMascotasComponent },
+  {path: 'editarMascota/:id', component: AddEditMascotaComponent },
+  {path: '**', redirectTo: 'listaMascotas', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
