765d5e938c434d66d185c19f8f1b82f51856be8e
Andres Ochoa <andressochoaa@gmail.com>
2023-06-15 20:46:29 -0500

    urls a endpoint/api

--- src/app/services/mascota.service.ts
+++ src/app/services/mascota.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MascotaService {
 
+  AplicacionUrl: string = environment.endpoint;
+  ApiUrl: string = 'api/Mascota/';
+  
   constructor() { }
 }
